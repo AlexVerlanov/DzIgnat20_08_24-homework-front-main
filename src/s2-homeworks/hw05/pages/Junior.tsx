1a56c56e78ae261f9358b115ad9aced26509c34d
Alex Verlanov <phanton7487@gmail.com>
2026-09-13 19:51:08 +0300

    fix: move homework 10 to junior plus

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import HW8 from "../../hw08/HW8";
 import HW7 from "../../hw07/HW7";
 import HW9 from "../../hw09/HW9";
-import HW10 from "../../hw10/HW10";
-// import HW6 from '../../hw06/HW6'
-// import HW7 from '../../hw07/HW7'
-// import HW8 from '../../hw08/HW8'
-// import HW9 from '../../hw09/HW9'
 
 function Junior() {
   return (
@@ -15,7 +10,6 @@ function Junior() {
       <HW7 />
       <HW8 />
       <HW9 />
-      <HW10 />
     </div>
   );
 }
